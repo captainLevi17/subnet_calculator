2373667da91cc48d44c2ff9c22fb576f66c369f7
captainLevi17 <migz.levi@gmail.com>
2026-03-17 23:53:26 +1100

    Learned about the grid system and buttons

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from tkinter import *
 
 root = Tk()
 
-# Creating a label widget
-myLabel = Label(root, text="MLSG's Subnet Calculator", font=("Arial", 16, "bold"))                                                                                       
-# Shoving it onto the screen
-myLabel.pack()
+def calculate():
+    result = Label(root, text="Calculating...")
+    result.grid(row=2, column=1)
+    
+
+header = Label(root, text="MLSG Subnet Calculator", fg="blue", font=("Helvetica", 16))
+result = Label(root, text="")
+calculateBtn = Button(root, text="Calculate", padx=10, pady=10 , command=calculate, fg="white", bg="blue")
+
+header.grid(row=0, column=1)
+calculateBtn.grid(row=1, column=1)
 
 root.mainloop()
